1b8745ea9516f78ab4f78ccf464fa02867032682
Aakash B <aakash.bamne@gmail.com>
2024-04-20 07:23:05 +0530

    removes commented code

--- social-media-profile-management/cmd/server/main.go
+++ social-media-profile-management/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go-projects/social-media-profile-management/internal/api"
 	"go-projects/social-media-profile-management/internal/middleware"
 	"go-projects/social-media-profile-management/pkg/log"
@@ -15,39 +14,6 @@ func main() {
 
 	// Set the logging level
 	logger.SetLevel(log.InfoLevel)
-
-	// Log messages at different levels
-	logger.Debug("This debug message will not appear in the log because the level is set to Info.")
-	logger.Info("This is an informational message.")
-	logger.Warn("This is a warning message.")
-	logger.Error("This is an error message.")
-
-	fmt.Println("Check your specified log file for output!")
-
-	// Example setup: Determine whether to use memory based on an environment variable
-	//useMemory := os.Getenv("USE_MEMORY_REPO") == "1"
-	//var db *sql.DB
-	//var err error
-	//
-	//if !useMemory {
-	//	// Setup database connection
-	//	db, err = sql.Open("mysql", "user:password@/dbname")
-	//	if err != nil {
-	//		logger.Error("Error opening database connection: ")
-	//	}
-	//	defer func(db *sql.DB) {
-	//		err := db.Close()
-	//		if err != nil {
-	//			logger.Error("Error closing database connection: ")
-	//		}
-	//	}(db)
-	//}
-	//
-	//// Use the factory to get the appropriate repository
-	//userRepository := repo.UserRepositoryFactory(useMemory, db)
-
-	// Setup services using the repository
-	//_ = service.NewUserService(userRepository)
 
 	logger.Info("Service setup complete.")
 	// Create a new ServeMux
